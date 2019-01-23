0f411dc77293e8f005ea299ab03e3c82b26f57e8
Michelle Liu <yifei.liu@epfl.ch>
2019-01-23 15:37:07 +0100

    script to fit charge vs time curve to exponential

---
+++ tools/expfit-charge-vs-time.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+data=np.loadtxt("netcharge")
+def func(x, a, b):
+   return a*(1-np.exp(-x/b))
+xdata=data[:,0]
+ydata=data[:,4]
+popt, pcov = curve_fit(func, xdata, ydata)
+
+plt.plot(xdata, ydata, 'b-', label='data')
+plt.plot(xdata, func(xdata, *popt), 'r-', label='fit:  a=%1.6e, b=%1.6e' % tuple(popt))
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.savefig("netcharge_expfit.png")
+
+np.savetxt("charging_expfit_params",popt)
+np.savetxt("charging_expfit_pcovar",pcov)
